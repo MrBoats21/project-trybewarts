7c050e8907eb3d43c79d9677580cc67dd6cd9fb1
brenusaraujo <brenoaraujo50@icloud.com>
2022-05-05 20:17:38 +0100

    teste

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-.
+function selectHouse() {
+  const selectOptions = [
+    { text: 'Gitnória', value: 'Gitnória', id: 'gitnoria-house', innerHTML: 'Gitnória' },
+    { text: 'Reactpuff', value: 'Reactpuff', id: 'reactpuff-house', innerHTML: 'Reactpuff' },
+    { text: 'Corvinode', value: 'Corvinode', id: 'corvinode-house', innerHTML: 'Corvinode' },
+    { text: 'Pytherina', value: 'Pytherina', id: 'pytherina-house', innerHTML: 'Pytherina' },
+  ];
+
+  const select = document.getElementById('house');
+  for (let index = 0; index < selectOptions.length; index += 1) {
+    const option = document.createElement('option');
+    option.text = selectOptions[index].text;
+    option.value = selectOptions[index].value;
+    option.id = selectOptions[index].id;
+    option.innerHTML = selectOptions[index].innerHTML;
+    if (index === 0) {
+      option.selected = 'true';
+    }
+    select.appendChild(option);
+  }
+}
+
+window.onload = selectHouse();

bef4776bf8b4c96530d3148307c6d92cedae0596
Marcus Guimarães <mrenanrguimaraes@gmail.com>
2022-05-05 19:53:35 -0300

    Todos os requisitos obrigatórios completos

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ const login = document.querySelector('#login');
 const checkbox = document.querySelector('#agreement');
 const submit = document.querySelector('#submit-btn');
 
-login.addEventListener('click', hello);
-
 function hello() {
   const valorEmail = email.value;
   const valorSenha = password.value;
@@ -16,61 +14,12 @@ function hello() {
   }
 }
 
-function selectHouse() {
-  const selectOptions = [
-    { text: 'Gitnória', value: 'Gitnória', id: 'gitnoria-house', innerHTML: 'Gitnória' },
-    { text: 'Reactpuff', value: 'Reactpuff', id: 'reactpuff-house', innerHTML: 'Reactpuff' },
-    { text: 'Corvinode', value: 'Corvinode', id: 'corvinode-house', innerHTML: 'Corvinode' },
-    { text: 'Pytherina', value: 'Pytherina', id: 'pytherina-house', innerHTML: 'Pytherina' },
-  ];
-  const select = document.getElementById('house');
-  for (let index = 0; index < selectOptions.length; index += 1) {
-    const option = document.createElement('option');
-    option.text = selectOptions[index].text;
-    option.value = selectOptions[index].value;
-    option.id = selectOptions[index].id;
-    option.innerHTML = selectOptions[index].innerHTML;
-    if (index === 0) {
-      option.selected = 'true';
-    }
-    select.appendChild(option);
-  }
-}
+login.addEventListener('click', hello);
 
-function selectRate() {
-  const arrayRate = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  const rateRadio = document.querySelector('#rate-box');
-
-  for (let index = 0; index < arrayRate.length; index += 1) {
-    const divRate = document.createElement('div');
-    const inputRate = document.createElement('input');
-    inputRate.type = 'radio';
-    inputRate.name = 'rate';
-    inputRate.id = `ratio-${arrayRate[index]}`;
-    inputRate.value = arrayRate[index];
-
-    const label = document.createElement('label');
-    label.htmlFor = arrayRate[index];
-    label.innerHTML = arrayRate[index];
-
-    if (index === 0) {
-      inputRate.checked = true;
-    }
-    rateRadio.appendChild(divRate);
-    divRate.appendChild(inputRate);
-    divRate.appendChild(label);
-  }
-}
-
-checkbox.addEventListener("change", (event) => {
-  if (event .target.checked) {
+checkbox.addEventListener('change', (event) => {
+  if (event.target.checked) {
     submit.disabled = false;
   } else {
     submit.disabled = true;
   }
 });
-
-window.onload = function () {
-  selectHouse();
-  selectRate();
-};
